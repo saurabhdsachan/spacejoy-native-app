e782825f93f2114dbe447c2e020c00fa771a0b46
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-02-08 17:52:03 +0530

    profile page

--- src/screens/PreQuiz/Quiz4.js
+++ src/screens/PreQuiz/Quiz4.js
@@ -1,18 +1,17 @@
 /* eslint-disable import/no-unresolved */
 import { Block, Button, Radio, Text } from '@components/index';
 import { images, theme } from '@constants/index';
-import { MILD } from '@constants/theme';
 import QuizData from '@data/Quiz4';
+import { DesignSelectionContext } from '@utils/helpers/designSelectionContext';
 import React from 'react';
 import { Image, StatusBar, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { DesignSelectionContext } from '@utils/helpers/designSelectionContext';
 
 const { SIZES, COLORS } = theme;
 
-const { lamp } = images;
+const { q4 } = images;
 
 const Quiz3 = ({ navigation }) => {
   const { userDesignSelections } = React.useContext(DesignSelectionContext);
@@ -29,6 +28,7 @@ const Quiz3 = ({ navigation }) => {
   return (
     <Block color={COLORS.white} padding={[SIZES.safe + 200, SIZES.padding, 0, SIZES.padding]}>
       <StatusBar barStyle="dark-content" />
+      <Image source={q4} style={styles.imageBg} />
       <Text h2>When should your space be ready?</Text>
       <Text small>A timeline will help us work towards your goal</Text>
       <ScrollView bounces={false}>
@@ -54,8 +54,7 @@ const Quiz3 = ({ navigation }) => {
           </Block>
         ))}
       </ScrollView>
-      <Block color={MILD.yellow} style={styles.abstract} />
-      <Image source={lamp} style={styles.imageBg} />
+
       <LinearGradient colors={[COLORS.transparent, COLORS.white]} style={styles.bottomButtons}>
         <Block center row space="between">
           <Button ghost color={COLORS.white} size="sm" onPress={() => navigation.goBack()}>
@@ -96,22 +95,11 @@ const styles = StyleSheet.create({
     width: SIZES.width,
     padding: SIZES.padding,
   },
-  abstract: {
-    position: 'absolute',
-    height: SIZES.base * 20,
-    width: SIZES.base * 20,
-    right: 20,
-    top: 20,
-    overflow: 'hidden',
-    borderRadius: 100,
-  },
   imageBg: {
-    position: 'absolute',
+    ...StyleSheet.absoluteFill,
     resizeMode: 'cover',
     flex: 1,
-    height: 170,
-    width: 100,
-    top: 60,
-    right: 50,
+    height: SIZES.height,
+    width: SIZES.width,
   },
 });
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -10,39 +10,86 @@ const Profile = ({ navigation }) => {
   const { data } = React.useContext(AuthContext);
   const { name = '', email = '', picture = '', channel } = data;
   return (
-    <Block middle center color={COLORS.white}>
-      <Image source={images.onboarding1} resizeMode="cover" style={styles.designFeedImage} />
-      <Image
-        source={{ uri: picture || images.defaultAvatar }}
-        resizeMode="cover"
-        style={{
-          height: 150,
-          width: 150,
-          borderRadius: 75,
-          marginBottom: SIZES.padding / 2,
-          borderColor: COLORS.transparent,
-          borderWidth: 10,
-        }}
-      />
-      <Text h2 center mt2>
-        {name}
-      </Text>
-      <Text body center mb4 mt1>
-        {email}
-      </Text>
-      <Button color={COLORS.black} onPress={() => navigation.goBack()}>
-        <Text color={COLORS.white}>Back</Text>
-      </Button>
+    <Block color={COLORS.white}>
+      <Image source={images.onboarding2} resizeMode="cover" style={styles.designFeedImage} />
+      <Block flex={2} center color={COLORS.semiTransparent}>
+        <Block center row shadow card padding={SIZES.padding} color={COLORS.white} style={styles.container}>
+          <Image source={{ uri: picture || images.defaultAvatar }} resizeMode="cover" style={styles.profileImage} />
+          <Block>
+            <Text h2 capitalize>
+              {name}
+            </Text>
+            <Text small light>
+              {email}
+            </Text>
+          </Block>
+        </Block>
+      </Block>
+      <Block flex={1} padding={SIZES.padding}>
+        <Block row center color={COLORS.white} card shadow flex={0.75}>
+          <Block padding={SIZES.padding}>
+            <Text small color={COLORS.gray} center>
+              Designs
+            </Text>
+            <Text h1 center>
+              098
+            </Text>
+          </Block>
+          <Block padding={SIZES.padding}>
+            <Text small color={COLORS.gray} center>
+              Designs
+            </Text>
+            <Text h1 center>
+              098
+            </Text>
+          </Block>
+          <Block padding={SIZES.padding}>
+            <Text small color={COLORS.gray} center>
+              Followers
+            </Text>
+            <Text h1 center>
+              817
+            </Text>
+          </Block>
+        </Block>
+      </Block>
+      <Block flex={3} padding={SIZES.padding} row>
+        <Block flex={6}>
+          <Button ghost onPress={() => navigation.goBack()}>
+            <Text center>Share</Text>
+          </Button>
+        </Block>
+        <Block />
+        <Block flex={6}>
+          <Button gradient onPress={() => navigation.goBack()}>
+            <Text center color={COLORS.white}>
+              Follow
+            </Text>
+          </Button>
+        </Block>
+      </Block>
     </Block>
   );
 };
 
 const styles = StyleSheet.create({
   designFeedImage: {
-    borderRadius: SIZES.radius / 2,
-    height: '100%',
+    ...StyleSheet.absoluteFillObject,
+    height: 180,
     width: '100%',
   },
+  container: {
+    marginTop: 120,
+    width: SIZES.width * 0.9,
+    maxHeight: 120,
+  },
+  profileImage: {
+    height: 80,
+    width: 80,
+    borderRadius: SIZES.radius / 1.5,
+    marginRight: SIZES.padding,
+  },
+  buttonWrapper: {},
 });
 
 export default Profile;
