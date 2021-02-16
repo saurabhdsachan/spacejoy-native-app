a712bcf7837d40868f74cc36f2b9ee93f51b4a35
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-02-16 14:34:23 +0530

    Profile card updated

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -44,7 +44,7 @@ const Avatar = ({ uri, user }) => {
     <>
       {renderNameAndImage}
       <Portal>
-        <Modalize modalTopOffset={SIZES.height * 0.6} ref={modalizeRef}>
+        <Modalize modalTopOffset={SIZES.height * 0.4} ref={modalizeRef}>
           <ProfileCard user={user} uri={uri} />
         </Modalize>
       </Portal>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import { theme } from '@constants/index';
 import { elevationShadowStyle } from '@utils/styleHelper';
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import Block from './Block';
 
@@ -11,16 +11,14 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class Card extends Component {
-  render() {
-    const { color, style, children, ...props } = this.props;
-    const cardStyles = { ...styles.card, ...(style || {}) };
-    return (
-      <TouchableOpacity activeOpacity={0.8} {...props}>
-        <Block color={color || theme.COLORS.white} style={cardStyles}>
-          {children}
-        </Block>
-      </TouchableOpacity>
-    );
-  }
-}
+const Card = ({ color, style, children, ...props }) => {
+  const cardStyles = { ...styles.card, ...(style || {}) };
+  return (
+    <TouchableOpacity activeOpacity={0.8} {...props}>
+      <Block color={color || theme.COLORS.white} style={cardStyles}>
+        {children}
+      </Block>
+    </TouchableOpacity>
+  );
+};
+export default Card;
